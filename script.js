197cc14804de5d6264163f1f596c9df0dcb030fd
Thomas O'Brien <thomasobrien99@gmail.com>
2016-05-26 16:59:32 -0700

    refactor

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var app = angular.module('realReddit', ['angularMoment']);
+var app = angular.module('realReddit', ['angularMoment','ngAnimate']);
 
 app.controller('mainController', function($scope)
 {
